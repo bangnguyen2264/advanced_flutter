c5e3af84f102854766481ca4d51539b43c83fd01
bangnguyen2264 <2251120335@ut.edu.vn>
2025-01-03 15:03:37 +0700

    addind timer to splash screen

--- lib/data/my_app.dart
+++ lib/data/my_app.dart
@@ -1,5 +1,8 @@
+import 'package:clean_architecture/presentation/resources/routes_manager.dart';
+import 'package:clean_architecture/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class MyApp extends StatefulWidget {
   MyApp._internal(); // private named constructor
   int appState = 0; // app state variable
@@ -14,6 +17,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      theme: getApplicationTheme(),
+    );
   }
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:clean_architecture/presentation/resources/assets_manager.dart';
+import 'package:clean_architecture/presentation/resources/color_manager.dart';
+import 'package:clean_architecture/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,8 +13,38 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(
+        child: Image(
+          image: AssetImage(ImageAssets.splashLogo),
+        ),
+      ),
+    );
   }
 }
